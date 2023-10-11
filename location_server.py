32e19058b0b1c78588c43a34b00bf6d604d53f90
Rob Spectre <rob@brooklynhacker.com>
2023-10-11 14:45:45 -0400

    1) Added new test button for `send_sherlock_report` to myself. 2) Added logging configuration file for `location_server.py`. 3) Added new interval address check for `location_server.py` to control    the state Sherlock is in. 4) Added test coverage for `location_server.py`. 5) Added new `httpx` dependency in `tox.ini` that is required by    FastAPI's testing.

--- location_server.py
+++ location_server.py
@@ -1,21 +1,43 @@
+import os
+import subprocess
+import logging.config
+
 from typing import Optional
 
 from fastapi import FastAPI
 
 from pydantic import BaseModel
 
+import geocoder
+
 
 class Location(BaseModel):
     mode: int
-    alt: Optional[float]
-    track: Optional[float]
-    speed: Optional[float]
-    lat: Optional[float]
-    lon: Optional[float]
+    alt: Optional[float] = None
+    track: Optional[float] = None
+    speed: Optional[float] = None
+    lat: Optional[float] = None
+    lon: Optional[float] = None
 
 
 app = FastAPI()
+
 app.current_location = None
+app.interval = 1
+app.interval_limit = 300
+app.counter = 0
+app.current_state = None
+
+app.GOOGLE_API_KEY = os.environ.get('GOOGLE_API_KEY', None)
+
+logging.config.fileConfig('logging.conf', disable_existing_loggers=False)
+logger = logging.getLogger(__name__)
+
+try:
+    with open('/var/www/html/ReadState.txt') as file:
+        app.current_state = file.read()
+except Exception as e:
+    logger.error(f"Unable to read Sherlock state on startup: {e}")
 
 
 @app.get("/location")
@@ -27,4 +49,37 @@ async def location():
 async def create_location(location: Location):
     app.current_location = location
 
+    app.counter += app.interval
+    if app.counter > app.interval_limit and location.mode > 1:
+        app.counter = 0
+
+        update_sherlock_state(location)
+
     return app.current_location
+
+
+def update_sherlock_state(location: Location):
+    address = None
+
+    try:
+        address = geocoder.google([location.lat, location.lon],
+                                  method='reverse')
+    except Exception as e:
+        logger.error(f"Could not reverse address: {e}")
+
+    if address and address.state is not app.current_state:
+        try:
+            with open('/var/www/html/ReadState.txt', 'w') as file:
+                file.write(address.state)
+            app.current_state = address.state
+        except Exception as e:
+            logger.error(f"Could not write to Sherlock state file: {e}")
+
+        try:
+            subprocess.call(["./press_shortcut.sh",
+                             "F5",
+                             "Mozilla"])
+        except Exception as e:
+            logger.error(f"Could not refresh Sherlock window: {e}")
+
+    return address
---
+++ tests/test_location_server.py
@@ -0,0 +1,142 @@
+from unittest import TestCase
+from unittest.mock import patch
+from unittest.mock import mock_open
+
+from fastapi.testclient import TestClient
+
+from location_server import app
+from location_server import Location
+from location_server import update_sherlock_state
+
+mock_gps_current = {
+    'mode': 2,
+    'lat': 12.345,
+    'lon': 67.890,
+    'speed': 50,
+    'track': 0,
+    'climb': 0,
+    'alt': 1000
+}
+
+
+class MockLocation:
+    def __init__(self):
+        self.city = 'Gojradia',
+        self.state = 'NY'
+        self.county = 'Latveria'
+
+
+class TestLocationServer(TestCase):
+    def setUp(self):
+
+        self.client = TestClient(app)
+
+        self.location = Location(mode=mock_gps_current['mode'],
+                                 lat=mock_gps_current['lat'],
+                                 lon=mock_gps_current['lon'],
+                                 speed=mock_gps_current['speed'],
+                                 track=mock_gps_current['track'],
+                                 climb=mock_gps_current['climb'],
+                                 alt=mock_gps_current['alt'])
+
+        self.mock_file = mock_open(read_data='NY')
+
+        self.file_patcher = patch('builtins.open',
+                                  self.mock_file)
+        self.mock_open = self.file_patcher.start()
+
+    def tearDown(self):
+        self.file_patcher.stop()
+
+        app.current_location = None
+        app.interval = 1
+        app.interval_limit = 300
+        app.counter = 0
+        app.current_state = 'NY'
+
+    def test_location(self):
+        response = self.client.get('/location').json()
+
+        self.assertEqual(response, None)
+
+    def test_location_with_polling(self):
+        self.client.put('/location',
+                        json=mock_gps_current)
+
+        response = self.client.get('/location').json()
+
+        self.assertEqual(response['mode'],
+                         mock_gps_current['mode'])
+
+    @patch('location_server.update_sherlock_state')
+    def test_interval_limit_exceeded(self, mock_update):
+        mock_update.return_value = MockLocation()
+
+        app.interval_limit = 1
+
+        self.client.put('/location',
+                        json=mock_gps_current)
+        self.client.put('/location',
+                        json=mock_gps_current)
+
+        mock_update.assert_called_once_with(self.location)
+
+    @patch('location_server.geocoder.google')
+    def test_update_sherlock_state(self, mock_google):
+        mock_google.return_value = MockLocation()
+
+        result = update_sherlock_state(self.location)
+
+        self.assertEqual(result.state, 'NY')
+
+    @patch('location_server.geocoder.google')
+    def test_update_sherlock_state_google_error(self, mock_google):
+        mock_google.side_effect = Exception("Whoopsie!")
+
+        result = update_sherlock_state(self.location)
+        self.assertIsNone(result)
+
+    @patch('location_server.geocoder.google')
+    def test_update_sherlock_state_writing_state(self, mock_google):
+        mock_google.return_value = MockLocation()
+
+        app.current_state = 'CT'
+
+        result = update_sherlock_state(self.location)
+
+        self.assertEqual(result.state, 'NY')
+        self.assertEqual(app.current_state, 'NY')
+        self.mock_file().write.assert_called_with('NY')
+
+    @patch('subprocess.call')
+    @patch('location_server.geocoder.google')
+    def test_update_sherlock_state_file_error(self,
+                                              mock_google,
+                                              mock_subprocess):
+        location = MockLocation()
+        app.current_state = 'CT'
+
+        mock_google.return_value = location
+        mock_subprocess.return_value = None
+        self.mock_open.side_effect = Exception("Whoopsie!")
+
+        result = update_sherlock_state(self.location)
+        self.assertEqual(result, location)
+        mock_subprocess.assert_called_with(['./press_shortcut.sh',
+                                           'F5',
+                                           'Mozilla'])
+
+    @patch('subprocess.call')
+    @patch('location_server.geocoder.google')
+    def test_update_sherlock_state_window_error(self,
+                                                mock_google,
+                                                mock_subprocess):
+        location = MockLocation()
+        app.current_state = 'CT'
+
+        mock_google.return_value = location
+        mock_subprocess.side_effect = Exception("Whoopsie!")
+        self.mock_open.side_effect = Exception("Whoopsie!")
+
+        result = update_sherlock_state(self.location)
+        self.assertEqual(result, location)
